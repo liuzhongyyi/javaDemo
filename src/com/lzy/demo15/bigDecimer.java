f015bfc8970c3152f380e170f657e0b71da40c3c
liuzhongyi <1471614345@qq.com>
2019-08-09 16:22:06 +0800

    day6 bigdecimal 笔记新增

--- src/com/lzy/demo15/bigDecimer.java
+++ src/com/lzy/demo15/bigDecimer.java
@@ -5,9 +5,11 @@ import java.math.BigDecimal;
 public class bigDecimer {
     public static void main(String[] args) {
 
-        BigDecimal decimal = new BigDecimal("3.14159");
-        BigDecimal decimal1 = decimal.setScale(2, BigDecimal.ROUND_UP);
-        System.out.println(decimal1);
-        System.out.println("aaa");
+        BigDecimal decimal = new BigDecimal(3.136);
+        System.out.println(decimal.doubleValue()); // -->3.136
+        BigDecimal scale = decimal.setScale(2, BigDecimal.ROUND_HALF_DOWN);
+        System.out.println(scale.doubleValue()); // 产生新对象对新对象有效 -->3.14
+
+
     }
 }
